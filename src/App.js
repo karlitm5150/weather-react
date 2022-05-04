625a68fc519e5e9054ddcb405d6471803df755f4
karlitm5150 <karli@tm5150.com>
2022-05-04 09:59:27 -0400

    api integration

--- src/App.js
+++ src/App.js
@@ -1,143 +1,18 @@
 import React from "react";
-import WeatherSearch from "./WeatherSearch.js";
+import Weather from "./Weather.js";
+
 import './App.css';
 
 export default function App() {
-   let weatherData = {
-    city: "San Diego",
-    temperature: 20,
-    description: "Sunny",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
-    humidity: 25,
-    wind: 0
-  };  
   return (
     <div className="App">
       <div className="container">
-      <div className="weather-app">  
-      <form className="search-form">
-          <div className="row">
-            <div className="col-6">
-              <WeatherSearch />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="form-control btn btn-primary shadow-sm"
-              />
-            </div>
-            <div className="col-3">
-              <button
-                className="btn btn-success w-100"
-              >
-                Current
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-      <div>
-        <h2>
-          {weatherData.city}
-        </h2>
-        <p></p>
-        <div className="row">
-        <div className="col-3">
-          <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
-              className="float-left"
-            />
-          <h1>
-            {weatherData.temperature}{" "}
-            <span className="units">
-              °C | °F
-            </span>
-          </h1>
-        <div className="col-6">
-          <p>
-            {weatherData.humidity} <span></span>%<br />{weatherData.wind} <span></span> km/h
-          </p>
-        </div>
-        </div>
-        </div>
-      </div>
-
-
-       <div className="row">
-        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
-          <h3>
-            Sun<br />
-            <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
-              className="float-left"
-            />
-          </h3>
-          <p>
-            70°F / 51°F
-          </p>
-        </div>
-        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
-          <h3>
-            Mon <br />
-            <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
-              className="float-left"
-            />
-          </h3>
-          <p>
-            69°F / 51°F
-          </p>
-        </div>
-        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
-          <h3>
-            Tues<br />
-            <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
-              className="float-left"
-              />
-          </h3>
-          <p>
-            71°F / 52°F
-          </p>
-          </div>
-          <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
-            <h3>
-              Wed<br />
-              <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
-              className="float-left"
-              />
-            </h3>
-            <p>
-              73°F / 52°F
-            </p>
-          </div>
-          <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
-            <h3>
-              Thurs<br />
-              <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
-              className="float-left"
-              />
-            </h3>
-            <p>
-              70°F / 51°F
-            </p>
-          </div>
-        </div>
+     <Weather defaultCity="New York" />
         </div>
 
       <footer>
         This projet is created by Karli Thuen and is{" "}
-        <a href="https://github.com/karlitm5150/weather-react"
-        target="_blank">open-sourced on GitHub
+        <a href="https://github.com/karlitm5150/weather-react">open-sourced on GitHub
         </a> 
       </footer> 
 
--- src/Weather.js
+++ src/Weather.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import WeatherSearch from "./WeatherSearch.js";
 import "./Weather.css";
 
-export default function Weather() {
-     let weatherData = {
-    city: "San Diego",
-    temperature: 20,
-    description: "Sunny",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
-    humidity: 25,
-    wind: 0
-  };  
+export default function Weather(props) {
+    const [weatherData, setweatherData] = useState({ ready: false });
+    function handleResponse(response){
+        setweatherData({
+            ready: true,
+            iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+            description: response.data.weather[0].description,
+            date: "Wed 9:00 AM",
+            temperature: response.data.main.temp,
+            city: response.data.name,
+            wind: response.data.wind.speed,
+            humidity: response.data.main.humidity
+        });
 
-    return (
+    }
+    if (weatherData.ready) {
+        return (
     <div>
         <div className="Weather">  
         <form className="search-form">
@@ -38,26 +45,28 @@ export default function Weather() {
         </form>
       </div>
       <div>
-        <h2 className="city">
+        <h2>
           {weatherData.city}
         </h2>
+        <h3>{weatherData.date}</h3>
+        <h4 className="text-capitalize">{weatherData.description}</h4>
         <p></p>
         <div className="row">
         <div className="col-3">
           <img
-              src={weatherData.imgUrl}
+              src={weatherData.iconUrl}
               alt={weatherData.description}
               className="float-left"
             />
-          <h1>
-            {weatherData.temperature}{" "}
+          <h1 className="temperature">
+            {Math.round(weatherData.temperature)}{" "}
             <span className="units">
               °C | °F
             </span>
           </h1>
         <div className="col-6">
           <p>
-            {weatherData.humidity} <span></span>%<br />{weatherData.wind} <span></span> km/h
+            Humidity: {weatherData.humidity}%<br />Wind: {weatherData.wind}km/h
           </p>
         </div>
         </div>
@@ -70,8 +79,8 @@ export default function Weather() {
           <h3>
             Sun<br />
             <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Sunny"
               className="float-left"
             />
           </h3>
@@ -83,8 +92,8 @@ export default function Weather() {
           <h3>
             Mon <br />
             <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Sunny"
               className="float-left"
             />
           </h3>
@@ -96,8 +105,8 @@ export default function Weather() {
           <h3>
             Tues<br />
             <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Sunny"
               className="float-left"
               />
           </h3>
@@ -109,8 +118,8 @@ export default function Weather() {
             <h3>
               Wed<br />
               <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Sunny"
               className="float-left"
               />
             </h3>
@@ -122,8 +131,8 @@ export default function Weather() {
             <h3>
               Thurs<br />
               <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
+             src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="Sunny"
               className="float-left"
               />
             </h3>
@@ -133,5 +142,16 @@ export default function Weather() {
           </div>
         </div>
         </div>
-    )
+    );
+    } else {
+    const apiKey = "d7b1e8f747dbf2f8c5f9d850588e67f8";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading..."
+;
+    }
+
+    
 }
