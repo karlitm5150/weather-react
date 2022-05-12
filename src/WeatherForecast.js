051243105bc087cca7a00aef93d4aa99993cb21d
karlitm5150 <karli@tm5150.com>
2022-05-12 10:40:50 -0400

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -5,7 +5,7 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
-
+ 
     function handleResponse(response) {
         setForecast(response.data.daily);
         setLoaded(true);
