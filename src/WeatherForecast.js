ea350e8532ce6be9a29a5cba20e203ff828e6d3f
karlitm5150 <karli@tm5150.com>
2022-05-12 09:20:53 -0400

    duplicated dates even with API error

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -18,12 +18,30 @@ export default function WeatherForecast(props) {
         <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
           <WeatherForecastDay data={forecast[0]} />
         </div>
+        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
+          <WeatherForecastDay data={forecast[1]} />
+        </div>
+        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
+          <WeatherForecastDay data={forecast[2]} />
+        </div>
+        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
+          <WeatherForecastDay data={forecast[3]} />
+        </div>
+        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
+          <WeatherForecastDay data={forecast[4]} />
+        </div>
+        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
+          <WeatherForecastDay data={forecast[5]} />
+        </div>
+        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
+          <WeatherForecastDay data={forecast[6]} />
+        </div>
         </div>
         </div>
       );
     
     } else {
-      let apiKey = "d30388075151941972c2605c5f0003d7";
+      let apiKey = "cd183893dec8034d4cb5133e87e32f89";
       let longitude = props.coordinates.lon;
       let latitude = props.coordinates.lat;
       let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
