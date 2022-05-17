f5bc155af5a3eb4418801acab9a35bfda4efe606
karlitm5150 <karli@tm5150.com>
2022-05-17 09:59:25 -0400

    Made columns fit in one row

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -27,25 +27,25 @@ export default function WeatherForecast(props) {
       return (
         <div classname="WeatherForecast">
         <div className="row">
-        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
+        <div className="col-1 shadow p-3 mb-5 bg-body rounded border text-center">
           <WeatherForecastDay data={forecast[0]} />
         </div>
-        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
+        <div className="col-1 shadow p-3 mb-5 bg-body rounded border text-center">
           <WeatherForecastDay data={forecast[1]} />
         </div>
-        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
+        <div className="col-1 shadow p-3 mb-5 bg-body rounded border text-center">
           <WeatherForecastDay data={forecast[2]} />
         </div>
-        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
+        <div className="col-1 shadow p-3 mb-5 bg-body rounded border text-center">
           <WeatherForecastDay data={forecast[3]} />
         </div>
-        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
+        <div className="col-1 shadow p-3 mb-5 bg-body rounded border text-center">
           <WeatherForecastDay data={forecast[4]} />
         </div>
-        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
+        <div className="col-1 shadow p-3 mb-5 bg-body rounded border text-center">
           <WeatherForecastDay data={forecast[5]} />
         </div>
-        <div className="col-2 shadow p-3 mb-5 bg-body rounded border text-center">
+        <div className="col-1 shadow p-3 mb-5 bg-body rounded border text-center">
           <WeatherForecastDay data={forecast[6]} />
         </div>
         </div>
